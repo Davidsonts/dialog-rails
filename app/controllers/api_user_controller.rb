b904316e916c97aa591c1b3373092842c0094d6c
Davidson <davidsonts@outlook.com>
2020-09-26 18:53:10 -0300

    paginate api controller

--- app/controllers/api_user_controller.rb
+++ app/controllers/api_user_controller.rb
@@ -1,7 +1,9 @@
 class ApiUserController < ApplicationController
+  require 'will_paginate/array'
   before_action :set_api_user_service
+
   def index
-    @users = @user_service.all
+    @users = @user_service.all.paginate(page: params[:page], per_page: 25)
     ## render :json => users.to_a
   end
 
