d5d873103870332955d95d0c83958ff681d4fd9d
Davidson <davidsonts@outlook.com>
2020-09-27 13:24:23 -0300

    adicionar required

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
+  validates :mome, acceptance: true
+  validates :idade, acceptance: true
+  validates :sexo, acceptance: true
+  validates :uf, acceptance: true
+
   uf = CS.states(:br).map do |key, value| key end
   enum uf: uf
   enum sexo: [:masculino, :feminino, :outro]
