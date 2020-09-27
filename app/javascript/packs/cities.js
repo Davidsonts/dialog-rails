a155f9631cfd3c196751332ae91da17c8a7be59a
Davidson <davidsonts@outlook.com>
2020-09-27 19:50:08 -0300

    fix cities js

--- app/javascript/packs/cities.js
+++ app/javascript/packs/cities.js
@@ -1,20 +1,43 @@
-$(document).ready(function($) {
-  $(document).on('turbolinks:load', function() {
-    $('#cidade').hide()
-    $('select#user_uf').change(function () {
-      var input_state = $('select[name="user[cidade]"]'); //$(this);
-      $.getJSON('/cities/' + $(this).val(), function (data) {
-        console.log(1, data)
-        $('#cidade').show()
-        $('.cidade-input').hide()
+document.addEventListener("turbolinks:load", function(event) {
+  if (!event.data.timing.visitStart) {
+    // Initial page load
+    $(document).ready(function($) {
+      $('#cidade').hide()
+      $('select#user_uf').change(function () {
+        var input_state = $('select[name="user[cidade]"]'); //$(this);
+        $.getJSON('/cities/' + $(this).val(), function (data) {
+          console.log(1, data)
+          $('#cidade').show()
+          $('.cidade-input').hide()
+          
+          input_state.empty();
         
-        input_state.empty();
-      
-        $.each(data, function (i, value) {
-          var opt = '<option value="'+ value +'">' +  value + '</option>'
-          input_state.append(opt)
+          $.each(data, function (i, value) {
+            var opt = '<option value="'+ value +'">' +  value + '</option>'
+            input_state.append(opt)
+          });
         });
       });
-    }); 
-  });
-});
+    });
+  } else {
+    // Turbolinks page load
+    $(document).ready(function($) {
+      $('#cidade').hide()
+      $('select#user_uf').change(function () {
+        var input_state = $('select[name="user[cidade]"]'); //$(this);
+        $.getJSON('/cities/' + $(this).val(), function (data) {
+          console.log(1, data)
+          $('#cidade').show()
+          $('.cidade-input').hide()
+          
+          input_state.empty();
+        
+          $.each(data, function (i, value) {
+            var opt = '<option value="'+ value +'">' +  value + '</option>'
+            input_state.append(opt)
+          });
+        });
+      });
+    });
+  }
+})
